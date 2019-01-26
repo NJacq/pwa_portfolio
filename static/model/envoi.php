135a17e915a071d61df76ba58d4a4d9112fd28cd
NJacq <nicolas.j@codeur.online>
2019-01-26 02:19:49 +0100

    correction bug ouvrir dialer + formulaire  dans contact.vue

--- static/model/envoi.php
+++ static/model/envoi.php
@@ -1,3 +1,50 @@
 <?php
-?>      
+header("Access-Control-Allow-Origin: *");
+    // S'il y des données de postées
+   
+      // (1) Code PHP pour traiter l'envoi de l'email
+     
+      // Récupération des variables et sécurisation des données
+      
+      // $lastName = $_POST['lastName'];
+      // $firstName = $_POST['firstName'];
+      // $email = $_POST['email'];
+      // $message = $_POST['message'];
+      // echo $lastName;
+      // echo $firstName;
+      // echo $email;
+      // echo $message;
+     
+      // Variables concernant l'email
+     
+      if (!empty($_POST['lastName']) && !empty($_POST['firstName']) && !empty($_POST['message']) && !empty($_POST['email']))
+      {
+      var_dump('coucou');
+      $lastName = $_POST['lastName'];
+      $firstName = $_POST['firstName'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+       
+			$destinataire='nicolas.jacquot7@free.fr';
+			$sujet="Portfolio";
+			$message = str_replace("\n", '<br>', $message); //retour à ligne
+			$new_message = '<p>' . $message . '</p><p>Envoyé par ' . $lastName . ' '.$firstName.' dont l\'email est : ' . $email . '</p>'; 
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";  
+      $headers .= 'From: ' . $lastName. ' '.$firstName.'<' . $email . '>';  
+      
 
+      mail($destinataire,
+      $sujet,
+      $new_message,
+      $headers);
+
+  }	    
+    else
+    {
+      
+    }
+  		
+    ?>
+
+    
